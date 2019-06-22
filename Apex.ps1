79eefc9d866b1be3d86f8d867c93366d9bcf5304
Penn Oser <pennoser@gmail.com>
2019-06-22 11:28:11 -0500

    Updated Config Files

--- Apex.ps1
+++ Apex.ps1
@@ -1,11 +1,11 @@
 # Script Vars
-$ApexLauchOptions = '-novid +fps_max unlimited -threads 4 -high -fullscreen'
+$ApexLauchOptions = '-novid +fps_max unlimited -threads 4 -high -windowed +mat_letterbox_aspect_goal 1.33 +mat_letterbox_aspect_threshold 1.33'
 $DesktopHrz = 60
 $DesktopWidth = 1920
 $DesktopHeight = 1080
 $ApexHrz = 50
-$ApexScreenWidth = 1440
-$ApexScreenHeight = 1080
+$ApexScreenWidth = 1280
+$ApexScreenHeight = 960
 
 # .Net Hide Console
 Add-Type -Name Window -Namespace Console -MemberDefinition '
@@ -25,6 +25,10 @@ Function Hide-Console
 
 # Main Script To Start Apex
 Hide-Console
+Stop-Process -Name "CenterTaskbar"
 .\ChangeScreenResolution /w=$ApexScreenWidth /h=$ApexScreenHeight /d=0
+Start-Process -FilePath "C:\Users\r1ft\Documents\CenterTaskbar.exe"
 Start-Process -FilePath ${env:ProgramFiles(x86)}'\Origin Games\Apex\r5apex.exe' -ArgumentList $ApexLauchOptions -Wait 
+Stop-Process -Name "CenterTaskbar"
 .\ChangeScreenResolution /w=$DesktopWidth /h=$DesktopHeight /d=0
+Start-Process -FilePath "C:\Users\r1ft\Documents\CenterTaskbar.exe"
